af6630fc500bd5c50e5c66e6c4346b147c22a8c0
PhonikTop <askar.a.shakirov@gmail.com>
2023-11-15 02:43:36 +0300

    Added generator

---
+++ generator.py
@@ -0,0 +1,23 @@
+import random
+import string
+
+
+class Generator:
+    def __init__(self):
+        self.number_of_symbols = input("Введите длину пароля.")
+
+    def symbols(self):
+        symbols = string.ascii_letters + ''.join([chr(1040 + i) for i in range(32)])
+        password = ''.join(random.choice(symbols) for _ in range(int(self.number_of_symbols)))
+        return password
+
+    def all_sym(self):
+        symbols = string.ascii_letters + string.digits + string.punctuation + ''.join(
+            [chr(1040 + i) for i in range(32)])
+        password = ''.join(random.choice(symbols) for _ in range(int(self.number_of_symbols)))
+        return password
+
+    def signs(self):
+        symbols = string.punctuation
+        password = ''.join(random.choice(symbols) for _ in range(int(self.number_of_symbols)))
+        return password
