27f0d74c3f83d671d022aa66808adf821352c39c
PhonikTop <askar.a.shakirov@gmail.com>
2023-11-15 02:59:25 +0300

    Updated generator.py and menu.py

--- generator.py
+++ generator.py
@@ -7,17 +7,27 @@ class Generator:
         self.number_of_symbols = input("Введите длину пароля.")
 
     def symbols(self):
-        symbols = string.ascii_letters + ''.join([chr(1040 + i) for i in range(32)])
-        password = ''.join(random.choice(symbols) for _ in range(int(self.number_of_symbols)))
+        symbols = string.ascii_letters + "".join([chr(1040 + i) for i in range(32)])
+        password = "".join(
+            random.choice(symbols) for _ in range(int(self.number_of_symbols))
+        )
         return password
 
     def all_sym(self):
-        symbols = string.ascii_letters + string.digits + string.punctuation + ''.join(
-            [chr(1040 + i) for i in range(32)])
-        password = ''.join(random.choice(symbols) for _ in range(int(self.number_of_symbols)))
+        symbols = (
+            string.ascii_letters
+            + string.digits
+            + string.punctuation
+            + "".join([chr(1040 + i) for i in range(32)])
+        )
+        password = "".join(
+            random.choice(symbols) for _ in range(int(self.number_of_symbols))
+        )
         return password
 
     def signs(self):
         symbols = string.punctuation
-        password = ''.join(random.choice(symbols) for _ in range(int(self.number_of_symbols)))
+        password = "".join(
+            random.choice(symbols) for _ in range(int(self.number_of_symbols))
+        )
         return password
--- menu.py
+++ menu.py
@@ -1,40 +1,18 @@
-def Main_menu(n):
-    if n == 1:
-        # Code for generating password consisting only of digits
-        print("Functionality for generating password consisting only of digits")
-    elif n == 2:
-        print('Choose an alphabet.')
-        print('1. Russian')
-        print('2. English')
-        print('3. Both Russian and English')
-        a = int(input())
-        print('Thank you for using our password generator!')
-    elif n == 3:
-        # Code for generating password consisting of letters, digits, and special characters
-        print('Functionality for generating password consisting of letters, digits, and special characters')
-    elif n == 4:
-        # Code for generating password consisting only of special characters
-        print('Functionality for generating password consisting only of special characters')
-    elif n == 5:
-        # Code for generating author's recommended password
-        print("Functionality for generating author's recommended password")
-    else:
-        print("Invalid choice")
+from generator import Generator
 
 
-if __name__ == '__main__':
+def Main_menu():
     try:
-        print("""Hello! You have opened the password generator.
-    To select a function, enter its number.
-    1. Password consisting only of digits.
-    2. Password consisting only of letters.
-    3. Password consisting of letters, digits, and special characters.
-    4. Password consisting only of special characters.
-    5. Create a password recommended by the author.""")
-        choice = int(input("Enter your choice: "))
-        Main_menu(choice)
+        gen = Generator()
+        symbols = gen.symbols()
+        signs = gen.signs()
+        all_sym = gen.all_sym()
 
-    except ValueError as e:
-        print(f"Ошибка: {e}. Пожалуйста, введите положительное целое число для длины пароля.")
+        print(f"{symbols}\n" f"{signs}\n" f"{all_sym}\n")
+
     except Exception as e:
         print(f"Произошла ошибка: {e}. Пожалуйста, попробуйте еще раз.")
+
+
+if __name__ == "__main__":
+    Main_menu()
